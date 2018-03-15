211dd8f918a9fc6f6389547d73ebfa6700dd7990
jonasna <jonasna93@gmail.com>
2018-03-15 14:46:12 +0100

    Updated interfaces

--- SwagAttack/SwagAttack/Interfaces/IGame.cs
+++ SwagAttack/SwagAttack/Interfaces/IGame.cs
@@ -1,7 +1,9 @@
-﻿namespace SwagAttack.Interfaces
+﻿using System.Collections.Generic;
+
+namespace SwagAttack.Interfaces
 {
     public interface IGame
     {
-        
+        IReadOnlyCollection<IUser> UsersInGame { get; }
     }
 }
