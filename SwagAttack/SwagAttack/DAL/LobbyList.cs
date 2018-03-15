96150eb90f3465e7aa19ba3f9fab701cb426fd71
Katborg <s.katborg@gmail.com>
2018-03-15 17:25:54 +0100

    made classes

--- SwagAttack/SwagAttack/DAL/LobbyList.cs
+++ SwagAttack/SwagAttack/DAL/LobbyList.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SwagAttack.Models;
 
 namespace SwagAttack.DAL
 {
     static class LobbyList
     {
+        internal static List<Lobby> lobbyList = new List<Lobby>();
+
+        static void Add(Lobby lobby)
+        {
+            lobbyList.Add(lobby);
+        }
+
+        static List<Lobby> Getlist => lobbyList;
+
     }
 }
