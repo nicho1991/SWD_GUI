372e2a3976fa20ec1f70b7f6f68deb8572f8323b
Katborg <s.katborg@gmail.com>
2018-03-15 17:11:00 +0100

    add DAl and classes

--- SwagAttack/SwagAttack/DAL/GameList.cs
+++ SwagAttack/SwagAttack/DAL/GameList.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SwagAttack.DAL
 {
-    class GameList
+    static class GameList
     {
+
     }
 }
--- SwagAttack/SwagAttack/DAL/UserList.cs
+++ SwagAttack/SwagAttack/DAL/UserList.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SwagAttack.Models;
+
 namespace SwagAttack.DAL
 {
-    class UserList
+    static class UserList
     {
+        static List<User> userList = new List<User>();
+
+        static bool Add(User user)
+        {
+            if (!userList.Contains(user)) { 
+                userList.Add(user);
+                return true;
+            }
+            return false;
+        }
+
+        static bool validataUser(User user)
+        {
+            if (!userList.Contains(user))
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
