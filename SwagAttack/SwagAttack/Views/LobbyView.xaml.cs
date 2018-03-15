5534d3bd0c843bf6c76ccfe46c5046188543e53c
Gobbenobber <gobbenobber@gmail.com>
2018-03-15 16:54:17 +0100

    Edit lobbyview

--- SwagAttack/SwagAttack/Views/LobbyView.xaml.cs
+++ SwagAttack/SwagAttack/Views/LobbyView.xaml.cs
@@ -24,5 +24,10 @@ namespace SwagAttack.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
