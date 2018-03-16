a25141a1e8daa2225987754ca06f725b63bcbe77
Malthe <malthedj@hotmail.com>
2018-03-16 14:38:54 +0100

    updated pages to usercontrols

--- SwagAttack/SwagAttack/Views/LobbyView.xaml.cs
+++ SwagAttack/SwagAttack/Views/LobbyView.xaml.cs
@@ -20,7 +20,7 @@ namespace SwagAttack.Views
     /// <summary>
     /// Interaction logic for LobbyView.xaml
     /// </summary>
-    public partial class LobbyView : Page
+    public partial class LobbyView : UserControl
     {
         public LobbyView()
         {
@@ -35,17 +35,17 @@ namespace SwagAttack.Views
 
         private void Logud_OnClick(object sender, RoutedEventArgs e)
         {
-            NavigationService?.Navigate(new Uri("Views/PreLogIndView.xaml", UriKind.Relative));
+            //NavigationService?.Navigate(new Uri("Views/PreLogIndView.xaml", UriKind.Relative));
         }
 
         private void StartSpil_OnClick(object sender, RoutedEventArgs e)
         {
-            NavigationService?.Navigate(new Uri("Views/PostLogIndView.xaml", UriKind.Relative));
+            //NavigationService?.Navigate(new Uri("Views/PostLogIndView.xaml", UriKind.Relative));
         }
 
         private void Afbryd_OnClick(object sender, RoutedEventArgs e)
         {
-            NavigationService?.Navigate(new Uri("Views/PostLogIndView.xaml", UriKind.Relative));
+            //NavigationService?.Navigate(new Uri("Views/PostLogIndView.xaml", UriKind.Relative));
         }
     }
 }
